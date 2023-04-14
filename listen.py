b8a554a65fc2eeb17288b9ec0a2aaa26585b6e1d
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-04-14 20:39:12 +0200

    formatted code

--- listen.py
+++ listen.py
@@ -9,14 +9,17 @@ init()
 
 keys_pressed = []
 
+
 def retrieve_word() -> str | None:
     letters = "".join(keys_pressed)
     return tictacbot(letters)
 
+
 def paste_word(word: str):
     pyperclip.copy(word)
-    pyautogui.press('backspace', presses=len(keys_pressed) + 1)
+    pyautogui.press("backspace", presses=len(keys_pressed) + 1)
     pyautogui.hotkey("command", "v")
+
 
 def on_press(key):
     try:
@@ -28,29 +31,30 @@ def on_press(key):
         if key == keyboard.Key.esc:
             keys_pressed.clear()
             print(keys_pressed)
-            print('_' * 15)
-            print('Keys pressed:')
+            print("_" * 15)
+            print("Keys pressed:")
         if key == keyboard.Key.space:
-            print('Searching...')
+            print("Searching...")
             word_found = retrieve_word()
-            if(word_found and len(word_found)):
+            if word_found and len(word_found):
                 word_found_colored = Fore.GREEN + word_found + Style.RESET_ALL
-                print('Found: ' + word_found_colored)
+                print("Found: " + word_found_colored)
                 paste_word(word_found)
             else:
-                print(Fore.RED + 'Not found' + Style.RESET_ALL)
+                print(Fore.RED + "Not found" + Style.RESET_ALL)
             keys_pressed.clear()
-            print('_' * 15)
-            print('Keys pressed:')
+            print("_" * 15)
+            print("Keys pressed:")
 
     if len(keys_pressed):
         print(keys_pressed)
+
 
 listener = keyboard.Listener(on_press=on_press)
 
 listener.start()
 
-tprint('Tictacbot')
-print('Keys pressed:')
+tprint("Tictacbot")
+print("Keys pressed:")
 
 listener.join()
